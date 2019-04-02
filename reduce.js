71461ed0fa5fa229e2a611173ae6027dbdb66c12
Nastya Faizulina <just-fy@mail.ru>
2019-04-02 10:41:33 +0300

    add solution

---
+++ reduce.js
@@ -0,0 +1,10 @@
+// BEGIN (write your solution here)
+const reduce = (f, node, acc) => {
+  const newAcc = f(acc, node);
+  if (node.type === 'file') {
+    return newAcc;
+  }
+  return node.children.reduce((iAcc, n) => reduce(f, n, iAcc), newAcc);
+};
+export default reduce;
+// END
